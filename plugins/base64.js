4ed3ae35b60e6faf4cc11680f450782a2502837b
Glynn Bird <glynn.bird@gmail.com>
2020-07-08 14:16:57 +0100

    remove base64 plugin

--- plugins/base64.js
+++
@@ -1,9 +0,0 @@
-const letters = require('./letters.js')
-
-module.exports = (count) => {
-  if (!count) {
-    count = 5
-  }
-  const str = letters(count)
-  return Buffer.from(str).toString('base64')
-}
