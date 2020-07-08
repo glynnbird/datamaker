3bc1759fe245a9a7d309a62492451c8dcbbf4795
Glynn Bird <glynn.bird@gmail.com>
2020-07-08 13:22:21 +0100

    0.5.0

--- plugins/md5.js
+++
@@ -1,7 +0,0 @@
-const letters = require('./letters.js')
-const crypto = require('crypto')
-
-module.exports = (count) => {
-  const str = letters(100)
-  return crypto.createHash('md5').update(str).digest('hex')
-}
