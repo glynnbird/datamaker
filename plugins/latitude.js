78d31870ad3b296adb3bf094b0ff1b0edad791fb
Glynn Bird <glynn.bird@gmail.com>
2024-08-23 10:06:54 +0100

    1.0.1

--- plugins/latitude.js
+++ plugins/latitude.js
@@ -2,6 +2,9 @@ const float = require('./float.js')
 const cities = require('./includes/cities.js')
 
 module.exports = (cityName) => {
+  if (cityName) {
+    cityName = cityName.trim().toUpperCase()
+  }
   const city = cityName && cities[cityName] ? cities[cityName] : cities['DEFAULT']
   return float(city.bottomLeft.lat, city.topRight.lat, 4)
 }
--- plugins/longitude.js
+++ plugins/longitude.js
@@ -3,6 +3,9 @@ const float = require('./float.js')
 const cities = require('./includes/cities.js')
 
 module.exports = (cityName) => {
+  if (cityName) {
+    cityName = cityName.trim().toUpperCase()
+  }
   const city = cityName && cities[cityName] ? cities[cityName] : cities['DEFAULT']
   return float(city.bottomLeft.long, city.topRight.long, 4)
 }
