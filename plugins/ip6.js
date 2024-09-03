d84122ff44f1f837e07e03281121868c5e73a65c
Glynn Bird <glynn.bird@gmail.com>
2024-09-03 09:21:24 +0100

    added pool to ip6

--- plugins/ip6.js
+++ plugins/ip6.js
@@ -1,4 +1,6 @@
 const cache = require('../cache.js')
+const pool = []
+const poolSize = 1000
 const int = () => {
   return Math.floor(Math.random() * 65535)
 }
@@ -7,8 +9,14 @@ module.exports = (local) => {
   if (local === 'true') {
     ip6 = ['fc00', int(), int(), int(), int(), int(), int(), int()].map(x => x.toString(16)).join(':')
   } else {
-    ip6 = [int(), int(), int(), int(), int(), int(), int(), int()].map(x => x.toString(16)).join(':')
+    // create a pool of 1000 ips to pick from
+    if (pool.length === 0) {
+      for(let i = 0 ; i < poolSize; i++) {
+        ip6 = [int(), int(), int(), int(), int(), int(), int(), int()].map(x => x.toString(16)).join(':')
+        pool.push(ip6)
+      }
+    }
+    ip6 =  pool[Math.floor(Math.random() * poolSize)] 
   }
-  cache.set('ip6', ip6)
   return ip6
 }
