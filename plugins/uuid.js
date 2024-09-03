7ab75e3a0a092ce6d7157dbcdf848f039c1c88aa
Glynn Bird <glynn.bird@gmail.com>
2024-09-03 09:25:54 +0100

    use Node.js built-in UUID generator

--- plugins/uuid.js
+++ plugins/uuid.js
@@ -1,13 +1,5 @@
-const options = 'ABCDEFGHIJKLMNOPQRSTUVQXYZ0123456789'
+const crypto = require("crypto")
 
 module.exports = (length) => {
-  let id = ''
-  if (!length || length < 0) {
-    length = 16
-  }
-  for (let i = 0; i < length; i++) {
-    const r = Math.floor(Math.random() * options.length)
-    id += options[r]
-  }
-  return id
+  return crypto.randomUUID()
 }
